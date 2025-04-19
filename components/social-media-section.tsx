ea3651b9b96907078bb6e5b6b21196067fbb206f
Lionel Wilson <Lionel_Wilson@outlook.com>
2025-04-19 16:17:13 +0100

    update social media section

--- components/social-media-section.tsx
+++ components/social-media-section.tsx
@@ -5,32 +5,32 @@ import { Instagram } from 'lucide-react';
 const instagramPosts = [
   {
     id: 1,
-    imageUrl: "https://images.unsplash.com/photo-1516280440614-37939bbacd81",
+    imageUrl: "https://cdn.prod.website-files.com/6469d767492ea69c34c8827d/64e678078091e95a25ae3c26_LC.png",
     filter: "bg-green-500/20"
   },
   {
     id: 2,
-    imageUrl: "https://images.unsplash.com/photo-1515187029135-18ee286d815b",
+    imageUrl: "https://cdn.prod.website-files.com/6469d767492ea69c34c8827d/64e67817cf780e8c335be314_SOL%201.png",
     filter: "bg-red-500/20"
   },
   {
     id: 3,
-    imageUrl: "https://images.unsplash.com/photo-1529070538774-1843cb3265df",
+    imageUrl: "https://cdn.prod.website-files.com/6469d767492ea69c34c8827d/64e678220afe0aee80c2cb81_SOL%202.png",
     filter: "bg-yellow-500/20"
   },
   {
     id: 4,
-    imageUrl: "https://images.unsplash.com/photo-1542596594-649edbc13630",
+    imageUrl: "https://cdn.prod.website-files.com/6469d767492ea69c34c8827d/64e67848738e787c71ed31e4_SOL%203.png",
     filter: "bg-blue-500/20"
   },
   {
     id: 5,
-    imageUrl: "https://images.unsplash.com/photo-1544005313-94ddf0286df2",
+    imageUrl: "https://cdn.prod.website-files.com/6469d767492ea69c34c8827d/652971123d77b40dc2e20763_square%20image%201.jpg",
     filter: "bg-purple-500/20"
   },
   {
     id: 6,
-    imageUrl: "https://images.unsplash.com/photo-1557425493-6f90ae4659fc",
+    imageUrl: "https://cdn.prod.website-files.com/6469d767492ea69c34c8827d/652971c68c43b381016cd07e_square%202.jpg",
     filter: "bg-orange-500/20"
   }
 ];
@@ -46,14 +46,20 @@ export function SocialMediaSection() {
               Follow us on social<br />media to stay connected
             </h2>
           </div>
+          <a
+              href="https://www.instagram.com/pagasacentre/" // replace with your actual Instagram URL
+              target="_blank"
+              rel="noopener noreferrer"
+          >
           <Button 
             size="lg"
             variant="outline"
-            className="mt-6 md:mt-0 border-white text-white hover:bg-white hover:text-black"
+            className="mt-6 md:mt-0 border-black text-black hover:bg-green-600 hover:text-black"
           >
             <Instagram className="mr-2 h-5 w-5" />
             OUR INSTAGRAM
           </Button>
+          </a>
         </div>
 
         <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
